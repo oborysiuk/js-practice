a4ca23ac7fdac2a5ae22c9784f16dc8de9bb80ac
oborysiuk <olga.borysiuk2012@gmail.com>
2025-03-28 18:12:25 +0200

    Lesson 4. JavaScript primitive data types

---
+++ jsBasics/task1.js
@@ -0,0 +1,7 @@
+let a1 = 15;
+let a2 = 'word';
+let a3 = true;
+let a4 = null;
+let a5 = undefined;
+
+console.log (a1, a2, a3, a4, a5);
---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+let name1 = 'Lisa';
+let name2 = 'Tina';
+
+let greetings1 = 'Hello ' + name1 + ' and ' + name2 + '!'
+let greetings2 = `Hello ${name1} and ${name2}!`;
+
+console.log(greetings1);
+console.log(greetings2);
