a64df9a214d6c715f38b17968d190e3c03432520
oborysiuk <olga.borysiuk2012@gmail.com>
2025-04-04 12:19:01 +0300

    lesson 5. JavaScript conditions and loops

---
+++ jsConditionsLoops/task3.js
@@ -0,0 +1,13 @@
+let number = 6;
+
+for (let i=1; i<11; i++){
+    console.log (`${number} x ${i} = ${number*i}`);
+}
+
+
+let j = 1;
+do{
+    console.log (`${number} x ${j} = ${number*j}`);
+    j++;
+}
+while (j<11)
